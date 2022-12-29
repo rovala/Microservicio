9a57c7c986f36532f3678485a736d0e4339de57c
rovala <rovalasiul@gmail.com>
2022-12-29 18:19:23 -0500

    configuracion eureka

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -2,10 +2,12 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients   //otra forma diferente de RestTemplate de comunicacion entre microservicios, ACTIVANDO FEIGN
+@EnableFeignClients   //otra forma diferente de RestTemplate de comunicacion entre microservicios, ACTIVANDO FEIGN, en este caso solo para este microservicio que hace las llamadas a los demas microservicios
+@EnableDiscoveryClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
